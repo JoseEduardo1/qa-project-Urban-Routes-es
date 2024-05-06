b3f3b57798b6539394e1fea1baae3a6df905b1fa
JoseEduardo1 <89206590+JoseEduardo1@users.noreply.github.com>
2024-05-06 15:16:45 -0600

    first commit

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://029f5e89-1ed2-457a-8c61-c3ff8b9e57c2.serverhub.tripleten-services.com'
+urban_routes_url = 'https://cnt-f12ccf3e-fba8-4319-8194-ac114591f695.containerhub.tripleten-services.com?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
